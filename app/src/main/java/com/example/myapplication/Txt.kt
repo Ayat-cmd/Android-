b29a957168084ea2f21db15ed63f288c6b06b2ec
Ayat-cmd <ayatram4@gmail.com>
2021-10-19 16:28:00 +0300

    lab_1

--- app/src/main/java/com/example/myapplication/Txt.kt
+++ app/src/main/java/com/example/myapplication/Txt.kt
@@ -3,6 +3,6 @@ package com.example.myapplication
 class Txt {
 
     fun fil(str: String): List<String> {
-        return str.split("? ",". ",", "," ").filter { e -> e[0] == e[e.length-1] && e.length>1}
+        return str.lowercase().split("? ",". ",", "," ").filter { e -> e[0] == e[e.length-1] && e.length>1}
     }
 }
